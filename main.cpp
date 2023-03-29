d2d445a71fe396028a4ba0492c2fa3f5838c0762
YassinAdel19 <108794095+YassinAdel19@users.noreply.github.com>
2023-03-29 21:13:49 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 #include <lemon/list_graph.h>
 #include <lemon/bfs.h>
 #include <lemon/path.h>
+#include <lemon/dijkstra.h>
 
 
 
@@ -30,10 +31,21 @@ int nbNode(const ListDigraph &g, map<int, node_info> nodes, int id1, int id2){
     return bfs.dist(t);
 }
 
+double dijkstraLength(const ListDigraph &g,const ListDigraph::ArcMap<double> &length, map<int, node_info> nodes, int id1, int id2){
+    ListDigraph::NodeMap<double> dist(g);
+    node_info s_info = nodes[id1];
+    node_info t_info = nodes[id2];
+    ListDigraph::Node s = s_info.getNode();
+    ListDigraph::Node t = t_info.getNode();   
+    dijkstra(g, length).distMap(dist).run(s);
+    return dist[t];
+}
+
 int 
 main(int argc, char **argv) {
 
   ListDigraph g;
+  ListDigraph::ArcMap<double> length(g);
   Bfs<ListDigraph> bfs(g);
   map<int, node_info> nodes;
   map<ListDigraph::Arc, arc_info> arcs;
@@ -92,6 +104,7 @@ main(int argc, char **argv) {
             arc_info arc1 = arc_info(arc, id1, id2, distance);
             //cout<< " id1= " << arc1.getid1()<< " id2= " << arc1.getid2() << " Distance = "<< arc1.getDistance()<<" Street: "<<endl;
             arcs[arc] = arc1;
+            length[arc] = distance;
         }
     }
     
@@ -109,6 +122,9 @@ main(int argc, char **argv) {
 
     cout << "The number of nodes between the nodes 19791 and 50179 is "<<nbNode(g,nodes,19791, 50179)<< " nodes" << endl;
     cout << "The number of nodes between the nodes 73964 and 272851 is " <<nbNode(g,nodes,73964, 272851)<< " nodes" << endl;
+    
+    cout << "dist[t] = "<< dijkstraLength(g, length,nodes, 73964, 272851)<<endl;
+
 
     // Calculate weight of the shortest path between two Nodes  
     /*double length;
